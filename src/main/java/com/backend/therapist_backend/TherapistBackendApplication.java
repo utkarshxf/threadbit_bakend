e05e7ca4fc47e6ca5dbc2036726cb0e646e9d203
utkarsh <utkarshsinghrajput16@gmail.com>
2023-12-28 18:01:50 +0530

    fix: change cross origen

--- src/main/java/com/backend/therapist_backend/TherapistBackendApplication.java
+++ src/main/java/com/backend/therapist_backend/TherapistBackendApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-@CrossOrigin(originPatterns = "http://127.0.0.1:8080")
+//@CrossOrigin(originPatterns = "http://127.0.0.1:8080")
+@CrossOrigin(originPatterns = "my-server-new2.onrender.com:8080")
 @SpringBootApplication
 @EnableSwagger2
 public class TherapistBackendApplication {
