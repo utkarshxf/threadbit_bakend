9e647315c5082aadc17a13a034cf0861c8ac3bce
utkarsh <utkarshsinghrajput16@gmail.com>
2025-05-16 18:30:00 +0530

    fix: profile modify

--- src/main/java/com/backend/threadbit/ThreadBitBackendApplication.java
+++ src/main/java/com/backend/threadbit/ThreadBitBackendApplication.java
@@ -7,14 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.List;
 
 @CrossOrigin(originPatterns = "*")
 @SpringBootApplication
-@EnableSwagger2
 public class ThreadBitBackendApplication {
 
     public static void main(String[] args) {
